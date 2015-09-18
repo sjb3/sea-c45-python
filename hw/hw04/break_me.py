2bb0c22f5b82101ad26878daae77a2268e91599f
Justin <klofseattle@hotmail.com>
2015-09-18 15:42:20 -0700

    Revision

---
+++ hw/hw04/break_me.py
@@ -0,0 +1,22 @@
+def test_name_Error():
+	# History repeats itself, first as tragedy, second as farce.
+	#Karl Marx """
+	
+	clintonOrbushoryou()
+	
+
+def test_attribute_Error():
+	""" Look up at the stars and not down at your feet. Try to make sense of what you see, and wonder about what makes the universe exist. Be curious.
+	Stephen Hawking """
+
+	log.curiousgeorge	
+
+def test_type_Error():
+	"""We are in danger of destroying ourselves by our greed and stupidity. We cannot remain looking inwards at ourselves on a small and increasingly polluted and overcrowded planet.
+	Stephen Hawking"""
+
+	a=str(100)
+	b=int(bills)
+
+	x=a+b
+
