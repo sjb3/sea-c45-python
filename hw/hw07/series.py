eb4ab631e6ab4a78f20eccf5c69143e0e73ecfb7
Justin <klofseattle@hotmail.com>
2015-09-21 18:46:54 -0700

    hw07 hw 08 update

---
+++ hw/hw07/series.py
@@ -0,0 +1,55 @@
+# Part One 
+# test_fibonacci_series
+
+import math
+
+def fibonacci(n):
+
+	n=int(num)
+	
+	if n:
+	#if n = 0:
+	#	return 0
+	#elif n = 1:
+	#	return 1
+	#else:
+		return fibonacci((n-1)+f(n+1))
+
+# Part Two 
+# test_lucasnumbers
+
+def lucas(n):
+
+	n=int(num)
+
+	if n:
+	#if n = 2:
+	#	return 2
+	#elif n = 1:
+	#	return 1
+	#else:
+		return lucas((n-1)+l(n+1))
+
+# Part Three
+# sum_series
+
+def sum(n):
+
+	n=int(num)
+
+	
+	if n = 0:
+		print 0
+	elif n = 1:
+		print 1
+	else:
+		print ("It's Fibonacci series!",fibonacci((n-1)+f(n+1))
+
+			if n = 2:
+				print 2
+			elif n = 1:
+				print 1
+			else:
+				print ("It's Lucas numers!",lucas((n-1)+l(n+1))
+
+					
---
+++ hw/hw08/homework8/dna_analysis.py
@@ -0,0 +1,120 @@
+# Name: Justin Byun
+# SEA-C45:
+# HW 8: DNA Counting
+
+# This program reads DNA sequencer output and computes statistics, such as
+# the GC content.  Run it from the command line like this:
+#   python dna_analysis.py myfile.fastq
+
+
+###########################################################################
+### Libraries
+###
+
+# The sys module supports reading files, command-line arguments, etc.
+import sys
+
+
+###########################################################################
+### Read the nucleotides into a variable named seq
+###
+
+# You need to specify a file name
+if len(sys.argv) < 2:
+    print("You must supply a file name as an argument when running this program.")
+    sys.exit(2)
+# The file name specified on the command line, as a string.
+filename = sys.argv[1]
+# A file object from which data can be read.
+inputfile = open(filename)
+
+# All the nucleotides in the input file that have been read so far.
+seq = ""
+# The current line number (= the number of lines read so far).
+linenum = 0
+
+
+for line in inputfile:
+    linenum = linenum + 1
+    # if we are on the 2nd, 6th, 10th line...
+    if linenum % 4 == 2:
+        # Remove the newline characters from the end of the line
+        line = line.rstrip()
+        seq = seq + line
+
+
+###########################################################################
+### Compute statistics
+###
+
+# Total nucleotides seen so far.
+total_count = 0
+g_count = 0
+c_count = 0
+a_count = 0
+t_count = 0
+
+
+# Number of G and C nucleotides seen so far.
+gc_count = 0
+
+
+# for each base pair in the string,
+for bp in seq:
+    # increment the total number of bps we've seen
+    total_count = total_count + 1
+
+    # next, if the bp is a G or a C,
+    if bp == 'C' or bp == 'G':
+        # increment the count of gc
+        gc_count = gc_count + 1
+        g_count = g_count + 1
+        c_count = c_count + 1
+
+# divide the gc_count by the total_count
+gc_content = float(gc_count) / total_count
+
+# Print the answer
+print('GC-content:', gc_content)
+print ('G content', g_count)
+print ('C content', c_count)
+
+
+
+######################################################################
+""" HW08: Problem 4: Compute AT content
+"""
+
+total_count = 0
+at_count = 0
+
+
+# for each base pair in the string,
+for bp in seq:
+    # increment the total number of bps we've seen
+    total_count = total_count + 1
+
+    # next, if the bp is a A or a T,
+    if bp == 'A' or bp == 'T':
+        # increment the count of at
+        at_count = at_count + 1
+        a_count = a_count + 1
+        t_count = t_count + 1
+
+# divide the at_count by the total_count
+at_content = float(at_count) / total_count
+
+# Print the answer
+print('AT-content:', at_content)
+print ('A content', a_count)
+print ('T content', t_count)
+
+total_countVariables = g_count + c_count + a_count + t_count
+
+print ('Total count variables=', total_countVariables)
+
+print ("The length of the seq variables=", len(seq))
+print ('AT/GC ratio', at_content/gc_count)
+
+
+
