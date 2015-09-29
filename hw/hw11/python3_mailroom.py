7bc65f19c592c9110251efd3bddaa9092438d7c6
Justin <klofseattle@hotmail.com>
2015-09-28 17:43:43 -0700

    HW11

---
+++ hw/hw11/python3_mailroom.py
@@ -0,0 +1,67 @@
+""" 
+
+Welcome to Mailroom madness
+
+"""
+
+# list contains the history of previous donors 
+
+import list
+import send_a_Thankyou.txt
+	
+"""
+Dear send_a_Thankyou,
+
+Thank you so much for your kind donation of donation_amt. 
+We here at the Foundation for Homeless Whales greatly appreciate it. 
+Your money will go towards creating new oceans on the moon for whales to live in.
+
+Thanks again,
+
+Justin Byun
+
+Director, F.H.W.
+
+Press Enter to Continue...
+
+"""
+
+#donorName=fullName
+
+If send_a_Thankyou:
+
+	print 'fullName'
+
+	
+	elif 'list':
+			print 'donorName'
+			return 'send_a_Thankyou'
+	
+		else fullName not in list:
+			list.append(fullName)
+
+			if send_a_Thankyou:
+				print donation_amt
+
+			if donation_amt==int:
+					return fullName in list
+
+					print send_a_Thankyou
+
+					return
+	exit()
+
+
+
+### Create a report
+
+If create_Areport:
+
+	if create_Areport:
+		print('donorName',':','$','totalDonated',',', 'numberofDonation', '$','avgDonation')
+		
+		print send_a_Thankyou
+
+	quit()
+
+
